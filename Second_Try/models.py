ab8b882cb9860cb7b51fa91a8c35c415925f50b3
Ezekiel-Zhao <98224025+Ezekiel-Zhao@users.noreply.github.com>
2023-11-16 23:34:26 +0000

    pseudo phone encoder added

--- Second_Try/models.py
+++ Second_Try/models.py
@@ -617,7 +617,7 @@ class psudo_phoneme(nn.Module):
     
 
 
-    return text_padded
+    return text_padded, text_lengths
   
 class Conv1DEncoder(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
@@ -645,24 +645,44 @@ class Conv1DEncoder(nn.Module):
 
 class pseudo_text_encoder(nn.Module):
     
-    def __init__(self, n_vocab, inter_channels,
-                        hidden_channels,
-                        n_layers, kernel_size):
+    def __init__(self,
+      n_vocab,
+      out_channels,
+      hidden_channels,
+      filter_channels,
+      n_heads,
+      n_layers,
+      kernel_size,
+      p_dropout):
+        
         super().__init__()
         self.n_vocab = n_vocab
-        self.out_channels = inter_channels
+        self.out_channels = out_channels
         self.hidden_channels = hidden_channels
+        self.filter_channels = filter_channels
+        self.n_heads = n_heads
         self.n_layers = n_layers
         self.kernel_size = kernel_size
+        self.p_dropout = p_dropout
 
         self.emb = nn.Embedding(n_vocab, hidden_channels)
         nn.init.normal_(self.emb.weight, 0.0, hidden_channels**-0.5)
-        self.encoder = Conv1DEncoder(hidden_channels, inter_channels, kernel_size, 1, 0)
+        self.encoder = attentions.Encoder(
+                        hidden_channels,
+                        filter_channels,
+                        n_heads,
+                        n_layers,
+                        kernel_size,
+                        p_dropout)
+        self.proj= nn.Conv1d(hidden_channels, out_channels * 2, 1)
 
-    def forward(self, x): 
-        print(x.shape)
-        x = self.emb(x)
+    def forward(self, x, x_lengths): 
+        x = self.emb(x) * math.sqrt(self.hidden_channels)
         x = x.permute(0,2,1)
-        print(x.shape)
-        x =  self.encoder(x)
-        return x
+        x_mask = torch.unsqueeze(commons.sequence_mask(x_lengths, x.size(2)), 1).to(x.dtype)
+
+        x = self.encoder(x * x_mask, x_mask)
+        stats = self.proj(x) * x_mask
+
+        m, logs = torch.split(stats, self.out_channels, dim=1)
+        return x, m, logs, x_mask
